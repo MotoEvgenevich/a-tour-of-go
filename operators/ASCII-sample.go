afc2dcfd4cfa533e44e2111a2722fa673c8a867a
MotoEvgenevich <artemevgenevich89@gmail.com>
2022-03-30 10:46:23 +0300

    add ASCII-sample

---
+++ operators/ASCII-sample.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "Hello, 世界"
+	for i, c := range a {
+		fmt.Printf("%d: %s\n", i, string(c))
+	}
+	fmt.Println("lenght of 'Hello, 世界':", len(a))
+}
