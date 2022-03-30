93f784a8a6f7926b266f48d155fc8ab7bef71df6
MotoEvgenevich <artemevgenevich89@gmail.com>
2022-03-30 10:48:00 +0300

    add-range-strings

---
+++ operators/range-strings.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	names()
+}
+func names() {
+	fmt.Println("Enter your name:")
+	var name string
+	fmt.Scanln(&name)
+	// проверяем на глассные имя
+	for _, v := range strings.ToLower(name) {
+		if v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u' {
+			fmt.Println("Your name contains a vowel.")
+			return
+		}
+	}
+	fmt.Println("Your name does not contain a vowel.")
+}
