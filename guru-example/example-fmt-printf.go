911364819bb35f819a14697f3af0de795b003cee
MotoEvgenevich <artemevgenevich89@gmail.com>
2022-04-28 16:12:08 +0300

    add guru-examples-folder

---
+++ guru-example/example-fmt-printf.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%s -> %s\n", "a", "b")
+	fmt.Printf("%s 🚀 %s\n", "илон маск", "марс")
+	fmt.Printf("%s ошибка %s\n", 1, "b")
+	fmt.Printf("%d нет ошибки %d\n", 1, 2)
+}
